b6d621f8fc94f234aa50cd9580cc353a8a38d0d2
Pedro L Ramos <pedro.2.ramos@nokia.com>
2022-09-29 17:25:43 +0100

    Spell is almost done. Some bugs to be fixed though

--- cmd/Spell/main.go
+++ cmd/Spell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"github.com/trustmaster/go-aspell"
 	"log"
 	"os"
 	"strconv"
@@ -14,6 +15,8 @@ import (
 	"pedrolorgaramos.win/s/9fans-go/acme"
 )
 
+// TODO: add slang env variable to select language to spell
+
 var DocID = os.Getenv("winid")
 
 func main() {
@@ -22,13 +25,16 @@ func main() {
 	if windoc == nil {
 		log.Fatalf("Not running in acme")
 	}
+
 	var docname string
 	if t, err := windoc.ReadAll("tag"); err != nil || t[0] == ' ' {
 		docname = ""
 	} else {
 		docname = strings.Fields(string(t))[0]
 	}
+
 	q0, _, err := windoc.SelectionAddr()
+
 	winspell, err := acme.New()
 	if err != nil {
 		log.Fatalf("Could not open new acme window: %v", err)
@@ -42,53 +48,87 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not read content: %v", err)
 	}
-
 	r := bytes.NewReader(xdata)
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanRunes)
 
 	var (
-		wq0  int = q0
-		wq1  int = q0
+		// wq0 and wq1 marks the start and end of the word to be spellchecked
+		wq0 int = q0
+		wq1 int = q0
+
+		// word is the buffer which will contain the full word to be spellchecked
 		word strings.Builder
+
+		// scanner goes through xdata. It reads rune by rune
+		scanner *bufio.Scanner = bufio.NewScanner(r)
 	)
 
+	scanner.Split(bufio.ScanRunes)
 	for scanner.Scan() {
 		r, _ := utf8.DecodeRune(scanner.Bytes())
-		if unicode.IsSpace(r) {
+		if r != '-' && (unicode.IsSpace(r) || unicode.IsPunct(r)) {
 			// This only happens if text starts with space
 			if word.Len() == 0 {
 				wq1++
 				wq0 = wq1
 				continue
 			}
-			windoc.Addr("#%d,#%d", wq0, wq1)
+			if err := windoc.Addr("#%d,#%d", wq0, wq1); err != nil {
+				log.Fatalf("Error setting addr: %v", err)
+			}
 			windoc.Ctl("dot=addr")
-			spellcheck(winspell, word.String())
+			if fixedWrd, deltaQ := spellcheck(winspell, word.String()); !(fixedWrd == "" && deltaQ == 0) {
+				windoc.Write("data", []byte(fixedWrd))
+				wq1 += deltaQ
+			}
 			word.Reset()
+
+			wq1++
+			wq0 = wq1
 		}
-		if unicode.IsPrint(r) {
+		if unicode.IsLetter(r) || r == '-' {
 			word.WriteRune(r)
 			wq1++
-		}
-		if scanner.Err() == bufio.ErrFinalToken {
-			windoc.Addr("#%d,#%d", wq0, wq1)
-			windoc.Ctl("dot=addr")
-			spellcheck(winspell, word.String())
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("Failed to read word")
 	}
+	if fixedWrd, deltaQ := spellcheck(winspell, word.String()); !(fixedWrd == "" && deltaQ == 0) {
+		if err := windoc.Addr("#%d,#%d", wq0, wq1); err != nil {
+			log.Fatalf("Error setting addr: %v", err)
+		}
+		windoc.Write("data", []byte(fixedWrd))
+	}
 	winspell.Del(true)
 }
 
-func spellcheck(w *acme.Win, word string) {
+func spellcheck(w *acme.Win, word string) (fixedWrd string, deltaQ int) {
 	w.Addr(",")
 	w.Write("data", nil)
-	w.Fprintf("body", "Checking %s", word)
 	w.Ctl("clean")
 
+ 	speller, err := aspell.NewSpeller(map[string]string{
+		"lang": "en_US",
+	})
+	if err != nil {
+		log.Fatalf("could not open speller: %v", err)
+	}
+	defer speller.Delete()
+
+	w.Fprintf("body", "> %s\n", word)
+	w.Ctl("clean")
+
+	var sugglst []string
+	if speller.Check(word) {
+		return
+	} else if len(speller.Suggest(word)) < 5 {
+		sugglst = speller.Suggest(word)
+	} else {
+		sugglst = speller.Suggest(word)[:5]
+	}
+	
+	w.Fprintf("body", "%s\n", strings.Join(sugglst, "\n"))
+	w.Ctl("clean")
 	for e := range w.EventChan() {
 		switch e.C2 {
 		case 'x': // execute in tag
@@ -99,6 +139,17 @@ func spellcheck(w *acme.Win, word string) {
 				w.Del(true)
 				os.Exit(0)
 			}
+			if string(e.Text) == "Fix" {
+				fixedWrd = strings.TrimSpace(w.Selection())
+				deltaQ = len(fixedWrd) - len(word)
+				return
+			}
+		case 'X':
+			fixedWrd = strings.TrimSpace(string(e.Text))
+			deltaQ = len(fixedWrd) - len(word)
+			return
+
 		}
 	}
+	return
 }
